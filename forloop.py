1f8e8d7c6cd4ab5139f932a131a6e4179caf43a8
Pushp Gupta <pugpta2601@gmail.com>
2022-10-29 16:26:08 +0530

    edit

---
+++ forloop.py
@@ -0,0 +1,15 @@
+#for i in a:
+#    print(i)
+#
+#for i in range(0,10,2):
+#    print(i)
+
+#classes=["koc26","koc27","koc28"]
+#student=["pushp","prateek","utkarsh"]
+#for i in student:
+#    for j in classes:
+#        print(i,j)
+a=int(input())
+for i in range(1,a):
+    a+=i
+print(a)
--- practice.py
+++ practice.py
@@ -1,14 +1,7 @@
-import random
-print(random.randrange(0,1000))
-
-
-years_spent=int(input("how many years have you spent:"))
-salary=int(input("what's you current salary:"))
-if years_spent>10 :
-    print("your total bonus is:", salary+(salary*0.1))
-elif years_spent>6 and years_spent<10:
-    print("your total bonus is:",salary+(salary*0.08))
-elif years_spent<6:
-    print("your total bonus is:",salary+(salary*0.05))
-else:
-    print("bonus not applicable")
+i=1
+while i<=10:
+    if i%2==0:
+        print(i,"is an even number")
+    else:
+        print(i,"is an odd number")
+    i+=1

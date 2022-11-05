2ecc26561c0bc3efb623fd22954f48b5228369c0
Pushp Gupta <pugpta2601@gmail.com>
2022-11-05 09:08:31 +0530

    edit

--- array.py
+++ array.py
@@ -1,4 +1,14 @@
-a=10
-while a>=1:
-    print(a)
-    a-=1
+def isRectangle(a, b, c, d):
+    if (a == b and d == c) or (a == c and b == d) or (a == d and b == c):
+        return True
+    else:
+        return False
+# Driver code
+a= int(input())
+b= int(input())
+c=int(input())
+d=int(input())
+if isRectangle(a,b,c,d):
+    print("possible")
+else:
+    print("not possible")

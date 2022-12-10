312b510aadff3a953ec4a95e343fc2ef7e552c12
Pushp <pugpta2601@gmail.com>
2022-12-10 13:56:50 +0530

    edit

---
+++ config.py
@@ -0,0 +1,9 @@
+class laptop:
+    def __init__(self):
+        print("hello world")
+    def config(self):
+        print("HP VICTUS","i5 12th gen","256GB SSD","GTX1650")
+laptop1=laptop()
+laptop2=laptop()
+laptop2.config()
+laptop1.config()

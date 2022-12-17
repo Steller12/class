771b0b859d97ac445c44d932a3cf454047b19762
Pushp <pugpta2601@gmail.com>
2022-12-17 16:47:30 +0530

    edit

--- class.py
+++ class.py
@@ -75,27 +75,29 @@
 #         print("this is method 5")
 # obj=C()
 # obj.method3()
-class Bird:
-    def wings(self):
-        print("Bird has two wings")
-    def eyes(self):
-        print("Bird has two eyes")
-    def fly(self):
-        print("Most of the birds can fly")
+# class Bird:
+#     def wings(self):
+#         print("Bird has two wings")
+#     def eyes(self):
+#         print("Bird has two eyes")
+#     def fly(self):
+#         print("Most of the birds can fly")
     
-class Sparrow(Bird):
-    def fly(self):
-        print("Sparrow can fly")
+# class Sparrow(Bird):
+#     def fly(self):
+#         print("Sparrow can fly")
 
-class Ostrich(Bird):
-    def fly(self):
-        print("Ostrich can not fly")
+# class Ostrich(Bird):
+#     def fly(self):
+#         print("Ostrich can not fly")
 
-bird = Bird()
-sparrow = Sparrow()
-ostrich = Ostrich()
+# bird = Bird()
+# sparrow = Sparrow()
+# ostrich = Ostrich()
 
-bird.eyes()
-bird.wings()
-bird.fly()
-ostrich.fly()
+# bird.eyes()
+# bird.wings()
+# bird.fly()
+# ostrich.fly()
+# class car:
+#     pass

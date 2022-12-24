87a632d74723701abe16bc9ec841deface55b6b4
Pushp <pugpta2601@gmail.com>
2022-12-24 10:21:51 +0530

    edit

--- lastunit.py
+++ lastunit.py
@@ -4,3 +4,5 @@
 # print(f.read())
 # print(f.readline())
 f1=open("demo1.txt","w")
+f1.write("my name is Pushp Gupta \n")
+f1.write("iam pursuing my btech in computer science engineering from lovely proffessional university")
